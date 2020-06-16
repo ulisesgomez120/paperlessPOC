32cf468ff85fe8c9d976ec6908c614d7af319229
Ulises Gomez <ulisesgomez120@gmail.com>
2020-06-16 16:26:47 -0500

    removed state as it was not needed in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,36 +14,34 @@ const Title = styled.h1`
   margin-bottom: 24px;
 `;
 export default class Home extends Component {
-  state = {
-    workOrders: [
-      {
-        work_order_id: "WO3105",
-        material: "PACF 2.5kg 1.75mm",
-        batch_number: "2-200606",
-        qty_desired: 40,
-        run_date: "06/06/20",
-        line: "2",
-        qty_actual: 0,
-        date_created: "06/05/20",
-        time_start: "07:23:34",
-        time_finish: "14:04:43",
-      },
-      {
-        work_order_id: "WO3106",
-        material: "PACF 2.5kg 2.85mm",
-        batch_number: "2-200606",
-        qty_desired: 40,
-        run_date: "06/06/20",
-        line: "2",
-        qty_actual: 0,
-        date_created: "06/05/20",
-        time_start: "07:23:34",
-        time_finish: "14:04:43",
-      },
-    ],
-  };
+  workOrders = [
+    {
+      work_order_id: "WO3105",
+      material: "PACF 2.5kg 1.75mm",
+      batch_number: "2-200606",
+      qty_desired: 40,
+      run_date: "06/06/20",
+      line: "2",
+      qty_actual: 0,
+      date_created: "06/05/20",
+      time_start: "07:23:34",
+      time_finish: "14:04:43",
+    },
+    {
+      work_order_id: "WO3106",
+      material: "PACF 2.5kg 2.85mm",
+      batch_number: "2-200606",
+      qty_desired: 40,
+      run_date: "06/06/20",
+      line: "2",
+      qty_actual: 0,
+      date_created: "06/05/20",
+      time_start: "07:23:34",
+      time_finish: "14:04:43",
+    },
+  ];
 
-  workOrders = this.state.workOrders.map(({ workOrderId, line, material }) => {
+  workOrders = this.workOrders.map(({ workOrderId, line, material }) => {
     return (
       <RunBtn key={workOrderId}>
         <span>Line: {line}</span>
