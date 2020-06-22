87add904daaa0a768e243456f823d4386a9a82bf
ulisesgomez120 <ulisesgomez120@gmail.com>
2020-06-22 14:10:44 -0500

    added table to scrapTable>

--- src/styledComponents/ScrapTable.js
+++ src/styledComponents/ScrapTable.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
-const ScrapTable = (props) => {
-  return;
+const ScrapTable = props => {
+  return <div>hjlk</div>;
 };
 
 export default ScrapTable;
