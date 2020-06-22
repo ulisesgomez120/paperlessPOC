9b047318cbcbc625fb72be32376ac9f35d8844ab
ulisesgomez120 <ulisesgomez120@gmail.com>
2020-06-22 14:58:25 -0500

    added temp data and map throught data to create jsx

--- src/styledComponents/ScrapTable.js
+++ src/styledComponents/ScrapTable.js
@@ -1,12 +1,49 @@
 import React from "react";
 import styled from "styled-components";
 
-const ScrapTable = props => {
+const data = [
+  {
+    type: "B",
+    spoolNum: 1,
+    reason: "Start up",
+    weight: 2514,
+    operator: "ug"
+  },
+  {
+    type: "A",
+    spoolNum: 0,
+    reason: "Line break",
+    weight: 1456,
+    operator: "ug"
+  },
+  {
+    type: "A",
+    spoolNum: 3,
+    reason: "Contamination",
+    weight: 2532,
+    operator: "ug"
+  },
+  {
+    type: "A",
+    spoolNum: 4,
+    reason: "Contamination",
+    weight: 2493,
+    operator: "ug"
+  }
+];
+const tableJsx = data.map(obj => {
   return (
-    <table>
-      <tr></tr>
-    </table>
+    <tr>
+      <td>{obj.type}</td>
+      <td>{obj.spoolNum}</td>
+      <td>{obj.reason}</td>
+      <td>{obj.weight}</td>
+      <td>{obj.operator}</td>
+    </tr>
   );
+});
+const ScrapTable = props => {
+  return <table>{tableJsx}</table>;
 };
 
 export default ScrapTable;
